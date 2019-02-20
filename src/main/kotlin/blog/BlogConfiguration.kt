5e27584becb9ab39ad400b44962db7154feb28c0
shinyay <shinya.com@gmail.com>
2019-02-20 09:17:41 +0900

    Modify blog contents

--- src/main/kotlin/blog/BlogConfiguration.kt
+++ src/main/kotlin/blog/BlogConfiguration.kt
@@ -11,18 +11,18 @@ class BlogConfiguration {
     fun databaseInitializer(userRepository: UserRepository,
                             articleRepository: ArticleRepository) = ApplicationRunner {
 
-        val smaldini = userRepository.save(User("smaldini", "Stéphane", "Maldini"))
+        val syanagihara = userRepository.save(User("syanagihara", "Shinya", "Yanagihara"))
         articleRepository.save(Article(
-                title = "Reactor Bismuth is out",
-                headline = "Lorem ipsum",
-                content = "dolor sit amet",
-                author = smaldini
+                title = "1月の貿易収支、1兆4152億円の赤字　対中輸出17%減",
+                headline = "財務省が20日発表した1月の貿易統計速報（通関ベース）によると...",
+                content = "財務省が20日発表した1月の貿易統計速報（通関ベース）によると、輸出額から輸入額を差し引いた貿易収支は1兆4152億円の赤字だった",
+                author = syanagihara
         ))
         articleRepository.save(Article(
-                title = "Reactor Aluminium has landed",
-                headline = "Lorem ipsum",
-                content = "dolor sit amet",
-                author = smaldini
+                title = "マネー再び「緩和モード」　新興国債などに流入加速",
+                headline = "金融市場が再び「緩和相場」を試し始めた。米国の利上げ路線...",
+                content = "金融市場が再び「緩和相場」を試し始めた。米国の利上げ路線修正を起点に緩和的な金融政策が広がり、資金の調達環境が緩んでいる",
+                author = syanagihara
         ))
     }
 }
