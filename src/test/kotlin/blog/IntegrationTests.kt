94857185729facc1f4a76a667e36a74a4e046a33
shinyay <shinya.com@gmail.com>
2019-02-20 09:34:26 +0900

    Modify test for blog contents

--- src/test/kotlin/blog/IntegrationTests.kt
+++ src/test/kotlin/blog/IntegrationTests.kt
@@ -23,16 +23,16 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 		println(">> Assert blog page title, content and status code")
 		val entity = restTemplate.getForEntity<String>("/")
 		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-		assertThat(entity.body).contains("<h1>Blog</h1>", "Reactor")
+		assertThat(entity.body).contains("<h1>Blog</h1>")
 	}
 
 	@Test
 	fun `Assert article page title, content and status code`() {
 		println(">> Assert article page title, content and status code")
-		val title = "Reactor Aluminium has landed"
+		val title = "1月の貿易収支、1兆4152億円の赤字　対中輸出17%減"
 		val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
 		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-		assertThat(entity.body).contains(title, "Lorem ipsum", "dolor sit amet")
+		assertThat(entity.body).contains(title, "財務省が20日発表した1月の貿易統計速報")
 	}
 
 	@AfterAll
